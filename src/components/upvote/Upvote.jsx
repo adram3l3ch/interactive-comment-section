47de0d30615fd691df4bf6c086e38d3a3beded78
adramelech <adramelech.psd@gmail.com>
2022-03-10 20:27:48 +0530

    upvote real functionality

--- src/components/upvote/Upvote.jsx
+++ src/components/upvote/Upvote.jsx
@@ -7,11 +7,17 @@ export const Upvote = ({ count }) => {
     const [state, setState] = useState(count);
     return (
         <div className="upvote">
-            <button onClick={() => setState(state + 1)} aria-label="upvote">
+            <button
+                onClick={() => state === count && setState(state + 1)}
+                aria-label="upvote"
+            >
                 <Plus />
             </button>
             <h3>{state}</h3>
-            <button onClick={() => setState(state - 1)} aria-label="downvote">
+            <button
+                onClick={() => state === count + 1 && setState(state - 1)}
+                aria-label="downvote"
+            >
                 <Minus />
             </button>
         </div>
